53ef4f529eab87ab56a18da931039f7c60716410
ARVIND_VERMA <153810511+arvindverma63@users.noreply.github.com>
2025-08-14 14:19:30 +0530

    Update fassai.blade.php
    write fassai modal code

--- resources/views/modals/fassai.blade.php
+++ resources/views/modals/fassai.blade.php
@@ -1,1 +1,20 @@
 
+<!-- Modal -->
+<div class="modal fade" id="fssaiModal" tabindex="-1" role="dialog" aria-labelledby="fssaiModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="fssaiModalLabel">FSSAI Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="fssaiData">
+        Loading...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-success" data-dismiss="modal">Accept</button>
+      </div>
+    </div>
+  </div>
+</div>
